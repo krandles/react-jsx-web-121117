b753c5a341f73a42f7983a6caf37eaf4d33753ae
krandles <krandles@gmail.com>
2018-02-17 14:57:04 -0500

    Done.

---
+++ src/Tweet.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Tweet extends React.Component {
+  render() {
+    return (
+      <div className='tweet'>
+        <img src="" className='tweet_avatar'/>
+        <div className="tweet_body">
+          <p>Blah</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Tweet;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// Code goes here
+import Tweet from './Tweet';
+// import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('root')
+)
